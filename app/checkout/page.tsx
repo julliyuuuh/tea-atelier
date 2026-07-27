441f5256feaeb9a15b5c3f8502e77f304f6e96ff
jan-bryy <concepcionjohnbry@gmail.com>
2026-07-27 16:15:56 +0800

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -61,6 +61,7 @@ export default function CheckoutPage() {
           province: formData.province,
           deliveryFee,
           paymentMethod,
+          phone: formData.phone,
         }),
       });
 
